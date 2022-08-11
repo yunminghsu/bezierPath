f412ffec91c621ce86e28c3611d5326699090d69
Arthur Ming <howard5816632@gmail.com>
2022-08-11 11:44:42 +0800

    Initial Commit

---
+++ bezierPath/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  bezierPath
+//
+//  Created by 許允銘 on 2022/8/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bezierPath/bezierPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  bezierPathApp.swift
+//  bezierPath
+//
+//  Created by 許允銘 on 2022/8/11.
+//
+
+import SwiftUI
+
+@main
+struct bezierPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
